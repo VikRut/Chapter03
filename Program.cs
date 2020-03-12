e85ca9037b2761a55ab5c5f09200309eaf475f7d
Viktor Vorobyev <Viktor.Vorobyev@erg.kz>
2020-03-12 14:01:35 +0600

    14:01

--- Program.cs
+++ Program.cs
@@ -80,9 +80,46 @@ namespace Chapter03
                 WriteLine("You did not enter a valid amount!");
             }
         }
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                return $"{number}th";
+                default:
+                string numberAsText = number.ToString();
+                char lastDigit= numberAsText[numberAsText.Length-1];
+                string suffix= string.Empty;
+                switch (lastDigit)
+                {
+                    case '1':
+                    suffix = "st";
+                    break;
+                    case '2':
+                    suffix = "nd";
+                    break;
+                    case '3':
+                    suffix = "rd";
+                    break;
+                    default:
+                    suffix = "th";
+                    break;
+                }   
+                return $"{number}{suffix}";
+            }
+        }
+        static void RunCardinalToOrdinal()
+        {
+            for (int number = 1; number <=40; number++)
+            {
+                WriteLine($"{CardinalToOrdinal(number)}");
+            }
+        }
         static void Main(string[] args)
         {
-            RunSalesTax();
+            RunCardinalToOrdinal();
         }
     }
 }
